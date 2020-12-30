c3c43d007a995d782c38e89faae1ef0210bcfaa1
unknown <renantielas@poli.ufrj.br>
2020-12-30 03:54:37 -0300

    inicio

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+
+
+const App = () =>{
+
+  return (
+
+    <View 
+    style={{flex:1 , justifyContent:'center', alignItems: 'center'}}>
+
+
+      <Text>
+        Agrofazer
+      </Text>
+
+
+    </View>
+
+
+
+  );
+};
+
+export default App;
